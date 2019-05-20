6cc143db18a7682a2814e92a40ec1b35985d88e2
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-20 09:21:27 -0400

    CRUD done

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,56 +1,23 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error mini message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error " : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} />
-        {this.renderError(meta)}
-      </div>
-    );
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error">
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui grey basic button">Submit</button>
-      </form>
+      <div>
+        <StreamForm onSubmit={this.onSubmit} title="Create a Stream" />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-  return errors;
-};
-
-export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+export default connect(
+  null,
+  { createStream }
+)(StreamCreate);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,146 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
+import styled from "styled-components";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderAdmin = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="icons">
+          <Link to={`/streams/edit/${stream.id}`}>
+            <i className="fas fa-pen" />
+          </Link>
+          <Link to={`/streams/delete/${stream.id}`}>
+            <i className="fas fa-trash" />
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  renderList = () => {
+    return this.props.streams.map(stream => {
+      return (
+        <Link
+          to={`streams/${stream.id}`}
+          className="ui raised card"
+          key={stream.id}>
+          <div className="content">
+            <div className="ui grid">
+              <div className="two wide column">
+                <i className="youtube huge red square icon" />
+              </div>
+              <div className="twelve wide column">
+                <div className="ui header">{stream.title}</div>
+                <div className="description">{stream.description}</div>
+              </div>
+              <div className="two wide column">{this.renderAdmin(stream)}</div>
+            </div>
+          </div>
+        </Link>
+      );
+    });
+  };
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui inverted green button">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <StreamListWrapper>
+        <h2>Streams</h2>
+        {this.renderCreate()}
+        <div className="ui stackable one cards">{this.renderList()}</div>
+      </StreamListWrapper>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
+
+const StreamListWrapper = styled.section`
+  .description {
+    color: var(--darkGrey);
+  }
+  .green {
+    margin-bottom: 30px;
+  }
+  .card {
+    min-height: 100px !important;
+    transition: var(--mainTransition) !important;
+  }
+  .fas {
+    padding: 10px;
+    width: 31px;
+    text-align: center;
+    transition: var(--mainTransition) !important;
+  }
+  .card:hover {
+    .fas {
+      display: block;
+    }
+  }
+  .header {
+    text-transform: capitalize;
+  }
+  .fa-pen,
+  .fa-trash {
+    padding: 7px;
+    color: #222;
+    border-color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 1px 5px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
+    margin: 5px;
+    display: none;
+    cursor: pointer;
+  }
+  .fa-trash {
+    background: #f55a5a;
+    color: #ffff;
+  }
+  @media (max-width: 768px) {
+    .header,
+    .description {
+      margin: 0 0 2px 10px !important;
+    }
+    .two {
+      margin-right: 20px;
+    }
+    h2 {
+      margin: 0 0 25px 15px;
+    }
+    .fas {
+      display: block;
+    }
+    .icons {
+      display: flex;
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+`;
